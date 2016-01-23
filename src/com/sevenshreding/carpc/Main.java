bd6b41b207898ac481b23751315868b79bb4613b
7ShredIng <sickguttural@directbox.com>
2016-01-23 02:21:15 +0100

    Test play audio from file.

--- src/com/sevenshreding/carpc/Main.java
+++ src/com/sevenshreding/carpc/Main.java
@@ -1,8 +1,23 @@
 package com.sevenshreding.carpc;
 
+import java.io.*;
+import sun.audio.*;
+
 public class Main {
 
 	public static void main(String[] args) {
+	
+	    String Filename = "//home//sick//Music//test.wav";
+	    try{
+		    InputStream in = new FileInputStream(Filename);
+		    AudioStream as = new AudioStream(in);         
+		    AudioPlayer.player.start(as);            
+		    //AudioPlayer.player.stop(as);
+	    }
+	    catch(Exception e)
+	    {
+	    	System.out.print(e);
+	    }
 		System.out.print("hello");
 	}
 }
